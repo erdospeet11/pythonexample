bdde7132876cbb34c4eb03325cb6459f56438fdb
Peter Erdos <erdospepe9@gmail.com>
2023-09-07 15:58:01 +0200

    full git commands

--- example.py
+++ example.py
@@ -4,9 +4,10 @@ szia = 11
 #github instructions
 
 #1. git init   letrehozza a gitet
-#2. git add .  hozzaadod az osszes fajlt a githez
+#2. git add .  hozzaadod a fajlt a githez
 #3. git branch -M main  main branchet kiválasztod
 #3. git remote add origin <link>  megadod a repo linket ahova akarsz tolteni
-#4 git push -u origin main
+#3. git commit -m "<comment>"     megcsinalja a gitet 
+#4 git push -u origin main        feltoltod a commitot
 
 print(hello + szia)
