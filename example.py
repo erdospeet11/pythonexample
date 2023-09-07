6b3647a9fa5f5a22148b41d2f24d15f08c998fdc
Peter Erdos <erdospepe9@gmail.com>
2023-09-07 15:56:04 +0200

    git instrukciok

--- example.py
+++ example.py
@@ -1,4 +1,12 @@
 hello = 10 
 szia = 11
 
+#github instructions
+
+#1. git init   letrehozza a gitet
+#2. git add .  hozzaadod az osszes fajlt a githez
+#3. git branch -M main  main branchet kiválasztod
+#3. git remote add origin <link>  megadod a repo linket ahova akarsz tolteni
+#4 git push -u origin main
+
 print(hello + szia)
